3f517ef5f557c25f645b5355e099ec8b5f0a7d9e
tyler <tylercharawell@gmail.com>
2021-03-31 19:39:36 -0700

    added mouseoverbackground color change

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const h2 = document.createElement('h2');
-h2.textContent = "Let's get to it!";
-document.querySelector('body').appendChild(h2);
+    h2.textContent = "Let's get to it!";
+        document.querySelector('body').appendChild(h2);
+document.getElementById("header").addEventListener("mouseover", function() {
+    document.getElementById("header").style.backgroundColor = "yellow";
+});
+document.getElementById("header").addEventListener("mouseout", function() {
+    document.getElementById("header").style.backgroundColor = "red";
+});
